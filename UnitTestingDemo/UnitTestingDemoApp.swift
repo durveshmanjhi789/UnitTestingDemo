9a687c7cb20950b758a1ada0a0d9878d03fc46f1
Durvesh Manjhi <mdurvesh789@gmail.com>
2025-10-03 11:59:04 +0530

    commit after add new code for run loop vs dispatchQueue

--- UnitTestingDemo/UnitTestingDemoApp.swift
+++ UnitTestingDemo/UnitTestingDemoApp.swift
@@ -9,10 +9,23 @@ import SwiftUI
 
 @main
 struct UnitTestingDemoApp: App {
+    @Environment(\.scenePhase) private var scenePhase
     var body: some Scene {
         WindowGroup {
             //ContentView()
             ToDoView()
+        }
+        .onChange(of: scenePhase) { oldPhase, newPhase in
+            switch newPhase {
+            case .active:
+                print("App is Active")
+            case .inactive:
+                print("App is Inactive")
+            case .background:
+                print("App is in Background")
+            @unknown default:
+                print("Unknown state")
+            }
         }
     }
 }
--- UnitTestingDemo/VIewModel/ToDoViewModel.swift
+++ UnitTestingDemo/VIewModel/ToDoViewModel.swift
@@ -5,17 +5,41 @@
 //  Created by Durvesh Manjhi on 24/09/25.
 //
 
-
+import Foundation
 import Combine
 class ToDoViewModel:ObservableObject{
     private var service:ToDoServiceProtocol
     @Published var tasks:[ToDoModel] = []
+    @Published var timer:Int = 0
+    @Published var timerCommon:Int = 0
+    @Published var isLoading : Bool = false
+    @Published var imageData: Data?
+    private var bag = Set<AnyCancellable>()
     
     init(service: ToDoServiceProtocol) {
         self.service = service
         self.tasks = service.fetchTask()
+        createTimer()
+        createTimerRunLoop()
     }
-    
+    func loadImage(){
+        isLoading = true
+        guard let url = URL(string: "https://picsum.photos/300/600") else {
+            isLoading = false
+            return
+        }
+        URLSession.shared.dataTaskPublisher(for: url)
+            .map{$0.data}
+            .replaceError(with: nil)
+            .receive(on: RunLoop.main) // UI delay when intract with UI
+            //.receive(on: DispatchQueue.main) // not any effect when we intract with ui
+            .sink { [weak self] in
+                if let data = $0{
+                    self?.imageData = data
+                }
+            }
+            .store(in: &bag)
+    }
     
     func addTask(_ name:String){
         guard !name.isEmpty else { return }
@@ -23,4 +47,21 @@ class ToDoViewModel:ObservableObject{
         tasks.append(myvm)
     }
     
+    func createTimer(){
+        let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        RunLoop.current.add(timer, forMode: .default)
+    }
+    func createTimerRunLoop(){
+        let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(runloopTimer), userInfo: nil, repeats: true)
+        RunLoop.current.add(timer, forMode: .common)
+    }
+    @objc func updateTimer(){
+        timer = timer + 1
+    }
+    @objc func runloopTimer(){
+        timerCommon = timerCommon + 1
+        if timerCommon == 5{
+            loadImage()
+        }
+    }
 }
